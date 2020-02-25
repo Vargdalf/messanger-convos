0ef5424653aa10d79916a97317acb744e529b0ac
Kacper Mieszala <24653160+Vargdalf@users.noreply.github.com>
2020-02-25 22:40:10 +0100

    Refined classes to hopefully final state (yeah, sure)

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import json
-from datetime import datetime
 
-# file = 'files/messages/inbox/MartynaLipinska_-dkdE-MIeg/message_1.json'
-file = 'files/messages/inbox/mateusznycz_0xu-u6ou3q/message_1.json'
+# file = 'files/messages/inbox/mateusznycz_0xu-u6ou3q/message_1.json'
+file = 'files/messages/inbox/AleksandraKogut_6TQX1Y0GGw/message_1.json'
 
 
 def string_escape(s, encoding='utf-8'):
@@ -13,13 +12,14 @@ def string_escape(s, encoding='utf-8'):
 
 
 class Conversation:
-    __slots__ = ['participants', 'messages', 'title', 'is_still_participant', 'thread_type', 'thread_path']
 
-    def __init__(self, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-
-        self.messages = [Message(**message) for message in self.messages]
+    def __init__(self, conversation):
+        self.participants: list = conversation['participants']
+        self.messages: list = [Message(message) for message in conversation['messages']]
+        self.title: str = conversation['title']
+        self.is_still_participant: bool = conversation['is_still_participant']
+        self.thread_type: str = conversation['thread_type']
+        self.thread_path: str = conversation['thread_path']
 
     def __len__(self):
         return len(self.messages)
@@ -29,25 +29,48 @@ class Conversation:
 
 
 class Message:
-    __slots__ = ['sender_name', 'timestamp_ms', 'content', 'type']
 
-    def __init__(self, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+    def __init__(self, message):
+        self.author: str = message['sender_name']
+        self.timestamp: int = message['timestamp_ms']
+        self.type: str = message['type']
+
+        # Content:
+        self.content = self.audio_files = self.videos = self.gifs = self.photos = self.sticker = None
+        if 'content' in message:
+            self.content: str = message['content']
+        if 'audio_files' in message:
+            self.audio_files: list = message['audio_files']
+        if 'videos' in message:
+            self.videos: list = message['videos']
+        if 'gifs' in message:
+            self.gifs: list = message['gifs']
+        if 'photos' in message:
+            self.photos: list = message['photos']
+        if 'sticker' in message:
+            self.sticker: dict = message['sticker']
+
+        # Can be on any message
+        self.reactions = None
+        if 'reactions' in message:
+            self.reactions: list = message['reactions']
 
     def __len__(self):
         return len(self.content)
 
     def __str__(self):
-        return f'[{self.timestamp_ms}] {self.sender_name}: {self.content}'
+        return f'[{self.timestamp}] {self.author}: {self.content}'
+
+    def __repr__(self):
+        return self.__str__()
 
 
 if __name__ == '__main__':
     with open(file) as f:
         data = json.load(f)
 
-    nycz_conv = Conversation(**data)
-    print(nycz_conv)
+    conv = Conversation(data)
 
-    for ms in nycz_conv.messages:
-        print(ms)
+    print(conv.title, conv.participants)
+    print(len(conv.messages))
+    print(conv.messages)
